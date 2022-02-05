56be0fe1bc3717f84d299d9e6ca79f6647dae5ae
Tom M G <devbytom@gmail.com>
2022-02-05 11:45:17 -0300

    Remove politicians crawler related files

--- scripts/__main__.py
+++
@@ -1,9 +0,0 @@
-from congress_crawler import CongressCrawler
-from markdown_builders import CongressMarkdownBuilder, SenateMarkdownBuilder
-from senate_crawler import SenateCrawler
-
-if __name__ == "__main__":
-    SenateCrawler().run()
-    SenateMarkdownBuilder().run()
-    CongressCrawler().run()
-    CongressMarkdownBuilder().run()
